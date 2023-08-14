6b58c413ed041d5ebe610f155e677f0a996c197c
Thomas GEORGE <thomas.george2@laposte.net>
2023-08-14 11:12:38 +0200

    transfert mac

---
+++ philo.h
@@ -0,0 +1,15 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+typedef struct s_data
+{
+	int		    nb_philo;
+	int		    tdie;
+	int		    teat;
+	int         tsleep;
+    int         nb_meal;
+    long long   t_start;
+    long        t_current;
+}		t_data;
+
+#endif
